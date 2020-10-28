1857804be2627b3cc39784f2f9a9871edf6434c7
jihuder <julioarenas85@gmail.com>
2020-10-27 22:45:21 -0500

    Prints text, strings and characters.

---
+++ text.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * printcha - print a char
+ * @c: char to print
+ * Return: always 1
+ */
+int printcha(va_list c)
+{
+	char ch = va_arg(c, int);
+
+	write(1, &ch, 1);
+	return (1);
+}
+/**
+ * printstr - printstring
+ * @s: string to print
+ * Return: bytes printed
+ */
+int printstr(va_list s)
+{
+	int len;
+	char *str;
+
+	str = va_arg(s, char *);
+	if (!str)
+		str = "(null)";
+	for (len = 0; str[len] != '\0'; len++)
+		write(1, &str[len], 1);
+	return (len);
+}
