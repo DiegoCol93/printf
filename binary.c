823a71dfd80d050c871964cb2699a2553eb330c2
Diego Lopez <dfelo93@gmail.com>
2020-10-27 23:20:02 -0500

    Prints a number in binary base.

---
+++ binary.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * printbin - Prints a number in a binary form.
+ *
+ * @b: Input number.
+ *
+ * Return: Number of bytes.
+ */
+int printbin(va_list b)
+{
+	int i, nbyte = 0;
+	unsigned int buff[1024], n;
+	char ch;
+
+	n = va_arg(b, int);
+	if (n < 1)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+	for (i = 0; n > 0; i++)
+	{
+		buff[nbyte] = (n % 2);
+		n = n / 2;
+		nbyte++;
+	}
+	for (i = nbyte - 1; i >= 0; i--)
+	{
+		ch = buff[i] + '0';
+		write(1, &ch, 1);
+	}
+	return (nbyte);
+}
